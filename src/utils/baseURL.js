ac8bcadc68d2c9edc7c5602f1dba8cf54d638049
hrmrakib <hrmrakibs@gmail.com>
2024-08-23 11:34:40 +0600

    swiper install

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,2 +1,2 @@
-// export const baseURL = "http://localhost:3000";
-export const baseURL = "https://gadget-plus-seven.vercel.app";
+export const baseURL = "http://localhost:3000";
+// export const baseURL = "https://gadget-plus-seven.vercel.app";
