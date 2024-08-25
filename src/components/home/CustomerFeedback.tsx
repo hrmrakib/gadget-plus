5d0a3db6b5ebf1155ebe10fbb0f774a0656cdbe9
hrmrakib <hrmrakibs@gmail.com>
2024-08-25 09:51:33 +0600

    add testimonial data

--- src/components/home/CustomerFeedback.tsx
+++ src/components/home/CustomerFeedback.tsx
@@ -1,86 +1,42 @@
-"use client";
-import Image from "next/image";
-import React, { useState } from "react";
-import { BiLeftArrowAlt, BiRightArrowAlt } from "react-icons/bi";
+import FeedbackCarousel from "./FeedbackCarousel";
 
-const slider = [
+const testimonials = [
   {
-    img: "/iphone15.jpg",
+    id: 1,
+    image: "/testimonial/img1.webp",
+    name: "John Doe",
+    designation: "Senior Software Engineer",
     reviewText:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero totam odio deleniti a modi nostrum soluta minima commodi consequuntur eveniet Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero totam odio deleniti a modi nostrum soluta minima commodi consequuntur eveniet Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero totam odio deleniti a modi nostrum soluta minima commodi consequuntur eveniet Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero totam odio deleniti a modi nostrum soluta minima commodi consequuntur eveniet!",
-    customerName: "Moinal Dola",
-    designation: "kaca mal",
-    rating: 2,
+      "This platform has significantly improved my productivity. The tools are intuitive, and the support team is incredibly responsive. Highly recommended!",
+    star: 5,
+    qoutePng: "/testimonial/quote.png",
   },
   {
-    img: "/iphone14vs15.jpg",
-    reviewText: "Lorem!",
-    customerName: "Sobur Mia",
-    designation: "Kaji",
-    rating: 5,
+    id: 2,
+    image: "/testimonial/img2.webp",
+    name: "Jane Smith",
+    designation: "Product Manager",
+    reviewText:
+      "I've been able to streamline our workflows thanks to the comprehensive features offered. The user experience is top-notch and the customer service is exceptional.",
+    star: 4,
+    qoutePng: "/testimonial/quote.png",
   },
   {
-    img: "/headphone.jpg",
+    id: 3,
+    image: "/testimonial/img3.webp",
+    name: "Emily Johnson",
+    designation: "UX Designer",
     reviewText:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero totam odio deleniti a modi nostrum soluta minima commodi consequuntur eveniet!",
-    customerName: "Moinal Dola",
-    designation: "kaca mal",
-    rating: 2,
+      "The attention to detail and commitment to user satisfaction is evident. This service has been a game-changer for our team.",
+    star: 5,
+    qoutePng: "/testimonial/quote.png",
   },
 ];
 
 const CustomerFeedback = () => {
-  const [source, setSource] = useState(slider);
-  const [index, setIndex] = useState(0);
-  const [currentSlider, setCurrentSlider] = useState(source[index]);
-
-  const handleGoSlider = () => {
-    // if (source.length < index) {
-    // return setIndex(0), setCurrentSlider(slider[0]);
-    // }
-
-    // console.log(source.length, index);
-
-    setIndex((prev) => prev + 1);
-    console.log({ index });
-    setCurrentSlider(slider[index]);
-  };
-
-  // console.log({ currentSlider, index });
-
   return (
-    <div className='w-[96%] mx-auto mt-20'>
-      <h2 className='text-3xl text-white t-shadow mb-8'>
-        OUR CUSTOMER FEEDBACK
-      </h2>
-      <div className='h-[300px] flex gap-10'>
-        <div className='min-w-[300px]'>
-          <Image
-            className='min-w-full min-h-full'
-            src={currentSlider?.img}
-            width={300}
-            height={300}
-            alt=''
-          />
-        </div>
-        <div className='relative bg-gray-800 p-8'>
-          <p className='text-white text-sm'>"{currentSlider?.reviewText}"</p>
-
-          {/* forward and backword */}
-          <div className='absolute bottom-0 left-5 flex'>
-            <div className='cursor-pointer left-1 bg-[#2043f1] hover:bg-[#163cf7ab] p-1 rounded'>
-              <BiLeftArrowAlt className='text-white text-xl' />
-            </div>
-
-            <div
-              onClick={handleGoSlider}
-              className='cursor-pointer right-1 bg-[#2043f1] hover:bg-[#163cf7a9] p-1 rounded'
-            >
-              <BiRightArrowAlt className='text-white text-xl' />
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
+      <FeedbackCarousel items={testimonials} />
     </div>
   );
 };
---
+++ src/components/home/FeedbackCarousel.jsx
@@ -0,0 +1,66 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import { BiLeftArrowAlt, BiRightArrowAlt } from "react-icons/bi";
+
+const FeedbackCarousel = ({ items }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrevious = () => {
+    const newIndex = currentIndex === items.length - 1 ? 0 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const handleNext = () => {
+    const newIndex = currentIndex === items.length - 1 ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  return (
+    <div className='w-[96%] mx-auto mt-20'>
+      <h2 className='text-3xl text-white t-shadow mb-8'>
+        OUR CUSTOMER FEEDBACK
+      </h2>
+      <div className='flex items-center gap-10 h-[300px]'>
+        <div className='min-w-[300px] max-w-[300px] h-[300px]'>
+          <Image
+            className='min-w-[300px] max-w-[300px] h-[300px]'
+            src={items[currentIndex]?.image}
+            width={300}
+            height={300}
+            alt=''
+          />
+        </div>
+        <div className='relative w-full h-[300px] bg-[#1c1c1c] p-8'>
+          <p className='text-white min-h-24'>
+            "{items[currentIndex]?.reviewText}"
+          </p>
+
+          <h2 className='text-2xl text-white'>{items[currentIndex]?.name}</h2>
+          <h3 className='text-blue-600 mt-2'>
+            {items[currentIndex]?.designation}
+          </h3>
+
+          {/* forward and backword */}
+          <div className='absolute bottom-5 left-8 flex'>
+            <div
+              onClick={handlePrevious}
+              className='cursor-pointer left-1 bg-[#2043f1] hover:bg-[#163cf7ab] p-1'
+            >
+              <BiLeftArrowAlt className='text-white text-xl' />
+            </div>
+
+            <div
+              onClick={handleNext}
+              className='cursor-pointer right-1 bg-[#2043f1] hover:bg-[#163cf7a9] p-1'
+            >
+              <BiRightArrowAlt className='text-white text-xl' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeedbackCarousel;
