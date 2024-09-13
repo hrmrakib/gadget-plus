f4a8c7652b9dc7f23a3c2597bdcf3f8c288b1f80
hrmrakib <hrmrakibs@gmail.com>
2024-09-13 08:15:08 +0600

    scroll: tredning category

--- src/components/home/TrendingCategories.tsx
+++ src/components/home/TrendingCategories.tsx
@@ -1,14 +1,47 @@
-import React from "react";
+"use client";
+
+import React, { useRef } from "react";
 import Image from "next/image";
 import { BiLeftArrowAlt, BiRightArrowAlt } from "react-icons/bi";
 
+const trending = [
+  {
+    id:1,
+    name: "",
+    image: ""
+  }
+]
+
 const TrendingCategories = () => {
+  const scrollContainerRef = useRef<HTMLDivElement | null>(null);
+
+  const scrollLeft = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({
+        left: -200,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({
+        left: 200,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
     <div className='relative'>
       <div className='mt-10 px-10 py-20'>
         <h2 className='text-3xl text-white t-shadow'>TrendingCategories</h2>
 
-        <div className='mt-10 flex gap-10 px-4 overflow-x-scroll scrollbar-zero'>
+        <div
+          ref={scrollContainerRef}
+          className='mt-10 flex gap-16 px-4 overflow-x-auto scrollbar-zero'
+        >
           {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((v, i) => (
             <div key={i}>
               <div className='bg-[#534848] rounded-full w-36 h-36 flex items-center justify-center'>
@@ -25,13 +58,19 @@ const TrendingCategories = () => {
           ))}
         </div>
         {/* forward and back */}
-        <div className='absolute top-1/2 cursor-pointer left-1 bg-[#2043f1] hover:bg-[#163cf7ab] p-1 rounded'>
+        <div
+          onClick={scrollLeft}
+          className='absolute top-1/2 cursor-pointer left-1 bg-[#2043f1] hover:bg-[#163cf7ab] p-1 rounded'
+        >
           <BiLeftArrowAlt className='text-white text-xl' />
         </div>
 
-        <div className='absolute top-1/2 cursor-pointer right-1 bg-[#2043f1] hover:bg-[#163cf7a9] p-1 rounded'>
+        <button
+          onClick={scrollRight}
+          className='absolute top-1/2 cursor-pointer right-1 bg-[#2043f1] hover:bg-[#163cf7a9] p-1 rounded'
+        >
           <BiRightArrowAlt className='text-white text-xl' />
-        </div>
+        </button>
       </div>
     </div>
   );
